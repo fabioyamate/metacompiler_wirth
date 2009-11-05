78025774550c070863f0ccb340189c65489c2ead
Fabio Yamate <fabioyamate@gmail.com>
2009-11-05 19:25:52 -0200

    Removing unused code.

--- app/controllers/wirth_controller.rb
+++ app/controllers/wirth_controller.rb
@@ -8,21 +8,21 @@ class WirthController < ApplicationController
   RULES = /.*\./
   RULE = /(?:([a-zA-Z]+)\s*=)?\s*(.+\.)/
   
-	def index
-		@wirth_notation = params[:wirth_notation]
-		return if @wirth_notation.nil?
-		
-		@converted = []
-		@automatas = {}
-		
-		@wirth_notation.gsub!(/(\342\200\230|\342\200\231|\342\200\234|\342\200\235)/, '"')
+  def index
+    @wirth_notation = params[:wirth_notation]
+    return if @wirth_notation.nil?
+    
+    @converted = []
+    @automatas = {}
+    
+    @wirth_notation.gsub!(/(\342\200\230|\342\200\231|\342\200\234|\342\200\235)/, '"')
     @wirth_notation.scan(RULES).each do |r|
       begin
         name, decl = RULE.match(r).captures
 
         s = Wirth.new(decl)
         s.execute
-    
+
         stated_rule = s.output.gsub(/\b(\d+)\b/, "<span>\\1</span>").gsub(/\b([A-Z][a-zA-Z]*)\b/, "<b>\\1</b>").gsub(/("[^\s]+")/, "<i>\\1</i>")
         @converted << { :name => name, :stated => stated_rule }
         @automatas[name] = fa_to_s(s.dfa)
@@ -31,24 +31,6 @@ class WirthController < ApplicationController
       rescue Exception => e
         @converted << { :name => name, :stated => "An unexpected error occurred" }
       end
-    end
-    
-	end
-	
-	def reverse_rename_nonterminals(str)
-	  str.scan(/[A-Z]/).uniq.each do |nt|
-	    str.gsub!(/\b#{nt}\b/, @dictionary.index(nt))
-    end
-    str
-  end
-	
-	def rename_nonterminals
-	  ch = 65 # 'A'
-    @dictionary = {}
-    @wirth_notation.scan(NONTERMINAL).uniq.each do |nt|
-      @dictionary[ch.chr] = nt
-      @wirth_notation.gsub!(/\b#{nt}\b/, ch.chr)
-      ch = ch + 1
     end
   end
 end
